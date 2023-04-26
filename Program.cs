c05bb43a8aa5a7005da8f6cee1bf8d50fadeeb10
Tanya Mo <yksheni@bk.ru>
2023-04-26 23:38:52 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("введите два числа");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+ if (a==b)
+    Console.WriteLine ($"a = {a}; b = {b} -> a=b");
+else {
+
+ if (a > b)
+    Console.WriteLine ($"a = {a}; b = {b} -> max = {a}");
+else 
+    Console.WriteLine ($"a = {a}; b = {b} -> max = {b}");
+}
